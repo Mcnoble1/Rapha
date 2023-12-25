fcdfdb107fe4cd328c1b972a63e888db196587a7
mcnoble <idowufestustemiloluwa@gmail.com>
2023-12-25 16:18:49 +0100

    feat: updated admin routes :octocat:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import { lazy } from 'react';
 
 const Doctor = lazy(() => import('../pages/Doctor/Dashboard'));
 const Patient = lazy(() => import('../pages/Patient/Dashboard'));
+const Admin = lazy(() => import('../pages/Admin/Dashboard'));
 const Patients = lazy(() => import('../pages/Doctor/Patients'));
 const Record = lazy(() => import('../pages/Doctor/Patient'));
 const Calendar = lazy(() => import('../pages/Doctor/Calendar'));
-// const Doctors = lazy(() => import('../pages/Patient/Doctors'));
-
+const Doctors = lazy(() => import('../pages/Patient/Doctors'));
+// const DoctorProfile = lazy(() => import('../pages/Patient/DoctorProfile'));
+// const PatientProfile = lazy(() => import('../pages/Doctor/PatientProfile'));
+const Docs = lazy(() => import('../pages/Admin/Doctors'));
 
 
 const coreRoutes = [
@@ -19,6 +22,21 @@ const coreRoutes = [
     path: '/patient',
     title: 'Patient',
     component: Patient,
+  },
+  {
+    path: '/admin',
+    title: 'Admin',
+    component: Admin,
+  },
+  {
+    path: '/admin/doctors',
+    title: 'Doctors',
+    component: Docs,
+  },
+  {
+    path: '/patient/doctors',
+    title: 'Doctors',
+    component: Doctors,
   },
   {
     path: '/doctor/patients',
