9d54f07b2f66c0cc3ab819be1d3041e84d737b27
mcnoble <idowufestustemiloluwa@gmail.com>
2023-12-27 16:28:21 +0100

    feat: updated admin dashboard :octocat:

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import Sidebar from '../../components/AdminSidebar.tsx';
 import Header from '../../components/Header.tsx';
 import Breadcrumb from '../../components/Breadcrumb';
-import CardFour from '../../components/CardFour.tsx';
-import CardOne from '../../components/CardOne.tsx';
-import CardTwo from '../../components/CardTwo.tsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons';
 
-const Health = () => {
+const Overview = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [customersData, setCustomersData] = useState<Customer[]>([]);
+  const [workersData, setWorkersData] = useState<Worker[]>([]);
+  const [usersData, setUsersData] = useState<User[]>(0);
+
+
 
   return (
     <div className="dark:bg-boxdark-2 dark:text-bodydark">
@@ -21,9 +26,53 @@ const Health = () => {
               <Breadcrumb pageName="Admin Dashboard Overview" />
               </div>
               <div className="grid grid-cols-2 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-              <CardOne />
-              <CardTwo />
-              <CardFour />
+                <div className="flex justify-between rounded-lg border border-stroke bg-white py-3 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="flex justify-between">
+                  <div>
+                  <span className="text-sm font-medium">Registered Doctors</span>
+                    <h4 className="text-title-sm text-black dark:text-white">
+                      {customersData.length}
+                    </h4>
+                  </div>
+                </div>
+        
+                <div className="flex h-11.5 w-11.5 items-center justify-center rounded bg-primary dark:bg-meta-4">
+                  <FontAwesomeIcon icon={faCircleUser} style={{color: "#ffffff",}} />
+                </div>
+              </div>
+              
+
+              <div className="flex justify-between rounded-lg border border-stroke bg-white py-3 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="flex justify-between">
+                <div>
+                <span className="text-sm font-medium">Registered Hospitals</span>
+                  <h4 className="text-title-sm text-black dark:text-white">
+                      {workersData.length}
+                  </h4>
+                </div>
+              </div>
+              
+              <div className="flex h-11.5 w-11.5 items-center justify-center rounded bg-primary dark:bg-meta-4">
+                  <FontAwesomeIcon icon={faUserGroup} style={{color: "#ffffff",}} />
+              </div>
+                </div>
+
+
+                <div className="flex justify-between rounded-lg border border-stroke bg-white py-3 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+                  <div className="flex justify-between">
+                  <div>
+                  <span className="text-sm font-medium">Medical Practitioners</span>
+                    <h4 className="text-title-sm text-black dark:text-white">
+                      {usersData}
+                    </h4>
+                  </div>
+                </div>
+                
+                <div className="flex h-11.5 w-11.5 items-center justify-center rounded bg-primary dark:bg-meta-4">
+                <FontAwesomeIcon icon={faCircleUser} style={{color: "#ffffff",}} />   
+                </div>
+                  </div>
+
             </div>
             </div>
           </main>
@@ -33,4 +82,4 @@ const Health = () => {
   );
 };
 
-export default Health;
+export default Overview;
