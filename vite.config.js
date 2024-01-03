6b6b12a37d5df2959b56a6b6e39feab41b112eb0
mcnoble <idowufestustemiloluwa@gmail.com>
2024-01-03 19:43:15 +0100

    feat: updated chat :octocat:

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
-/** @type {import('vite').UserConfig} */
-
 export default defineConfig({
   plugins: [react(), viteCommonjs()],
   build: {
